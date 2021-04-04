0282c514f7772507172bdcaed40705e9dc634374
Viktor Vilsky <vilsky.v@gmail.com>
2021-04-05 00:15:31 +0300

    add all the solutions for two sum algorithm

--- basic/sumOfTwo.js
+++ basic/sumOfTwo.js
@@ -1,3 +1,6 @@
+// My web-site http://upmate.io
+// YouTube channel - https://www.youtube.com/channel/UCYUUfSJ_8qEP9Nmo2SEXMUQ
+
 /*
 Given:
 arr - array of integers
@@ -14,39 +17,141 @@ sum = 10
 result = [3, 7]
  */
 
+// our arguments
+const arr = [1, 3, 5, 2, 6, 7, 5, 3, 10, 7]
+const sum = 10
+
 // Function complexity: O(n) time, O(n) space
-function sumOfTwo(arr, sum) {
+function twoSum(arr, sum) {
     // use Set to have O(1) time complexity for add() and has() methods
     const storage = new Set()
 
     // loop through all the given integers in the array
-    for (let digit of arr) {
+    for (let value of arr) {
         // calculate int we need to end up to a required sum
         // for the first iteration it'll be 10 - 1 = 9
-        const need = sum - digit
+        const need = sum - value
         // check if we have a needed integer in the storage
         if (storage.has(need)) {
             // if we have it - return our result with a pair
-            return [digit, need]
+            return [value, need]
+        }
+        // if we don't - add current number from iteration to the storage
+        storage.add(value)
+    }
+
+    // in case we found no pairs we return an empty array
+    return []
+}
+
+
+// Function complexity: O(n) time, O(n) space
+function twoSumAllPairs(arr, sum) {
+    // use Set to have O(1) time complexity for add() and has() methods
+    const storage = new Set()
+    // here we are going to accumulate our found pairs
+    const pairs = []
+
+    // loop through all the given integers in the array
+    for (let value of arr) {
+        // calculate int we need to end up to a required sum
+        // for the first iteration it'll be 10 - 1 = 9
+        const need = sum - value
+        // check if we have a needed integer in the storage
+        if (storage.has(need)) {
+            // if we have it - push to pairs array
+            pairs.push([value, need])
+            // delete needed value from the storage to avoid duplicates
+            storage.delete(need)
         } else {
             // if we don't - add current number from iteration to the storage
-            storage.add(digit)
+            storage.add(value)
         }
     }
 
-    // in case we found no pairs we return null
-    return null
+    // return all the collected pairs
+    return pairs
 }
 
-// our arguments
-const arr = [1, 3, 5, 2, 6, 7, 5]
-const sum = 10
+// Function complexity: O(n) time, O(n) space
+function twoSumAllPairsWithDuplicates(arr, sum) {
+    // use Set to have O(1) time complexity for add() and has() methods
+    const storage = new Set()
+    // here we are going to accumulate our found pairs
+    const pairs = []
 
-// call the function and get a result
-const result = sumOfTwo(arr, sum)
+    // loop through all the given integers in the array
+    for (let value of arr) {
+        // calculate int we need to end up to a required sum
+        // for the first iteration it'll be 10 - 1 = 9
+        const need = sum - value
+        // check if we have a needed integer in the storage
+        if (storage.has(need)) {
+            // if we have it - push to pairs array
+            pairs.push([value, need])
+        }
+        // if we don't - add current number from iteration to the storage
+        storage.add(value)
+    }
 
-// print the result to the console
-console.log(result)
+    // return all the collected pairs
+    return pairs
+}
 
-// My web-site http://upmate.io
-// YouTube channel - https://www.youtube.com/channel/UCYUUfSJ_8qEP9Nmo2SEXMUQ
+
+// Function complexity: O(n) time, O(n) space
+function twoSumIndexes(arr, sum) {
+    // use Set to have O(1) time complexity for add() and has() methods
+    const storage = new Map()
+    // here we are going to accumulate our found pairs
+    const pairs = []
+    // define length of the array out of the loop to gain efficiency
+    const arrLength = arr.length
+
+    // loop through all the given integers in the array
+    for(let i = 0; i < arrLength; i++) {
+        // check if we have a complement integer in the storage
+        if(storage.has(arr[i])) {
+            // if we have it - push to pairs array
+            pairs.push([storage.get(arr[i]), i])
+            // delete complement from the storage to avoid duplicates
+            storage.delete(arr[i])
+        } else {
+            // calculate current complement value
+            const complement = sum - arr[i]
+            // add current complement and its index from the iteration to the storage
+            storage.set(complement, i)
+        }
+    }
+
+}
+
+// our arguments sorted
+const arrSorted = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const target = 5
+
+// Function complexity: O(n) time, O(1) space
+function twoSumSorted(arr, target) {
+    // define array start index
+    let start = 0
+    // define array end index
+    let end = arr.length - 1
+
+    // loop until start === end
+    while (start !== end) {
+        // calculate sum of current values of start and end indexes
+        const sum = arr[start] + arr[end]
+
+        // if the sum is greater than the target - decrease the end index by 1
+        if (sum > target) end--
+        // if the sum is less than the target - increase the start index by 1
+        else if (sum < target) start++
+        // our sum equals the target value and we return found values
+        else return [arr[start], arr[end]]
+    }
+
+    // in case we found no pairs we return an empty array
+    return []
+
+}
+
